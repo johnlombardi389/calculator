566407ac6121c028199e0865ab973fba92a3b291
johnlombardi389 <lombardij38@gmail.com>
2022-08-15 11:28:54 -0400

    Added event listener to number buttons and tested display updates

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+class Calculator {
+  constructor(prevOperandText, currentOperandText) {
+    this.prevOperandText = prevOperandText;
+    this.currentOperandText = currentOperandText;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = '';
+    this.prevOperand = '';
+    this.operation = undefined;
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    this.currentOperandText.innerText = this.currentOperand;
+  }
+}
+
 // Select HTML elements
 const numberButton = document.querySelectorAll('[data-number]');
 const operationButton = document.querySelectorAll('[data-operation]');
@@ -6,3 +40,12 @@ const acButton = document.querySelector('[data-ac]');
 const deleteButton = document.querySelector('[data-delete]');
 const prevOperandText = document.querySelector('[data-prev-operand]');
 const currentOperandText = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(prevOperandText, currentOperandText);
+
+numberButton.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
